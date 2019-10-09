77c22ad0d464b2f379e8f564f7b0bfdb93010a81
cxxichen <chenxi920511@gmail.com>
2019-10-09 14:43:37 -0400

    create a config server

--- config-server/src/main/java/configserver/Application.java
+++ config-server/src/main/java/configserver/Application.java
@@ -1,4 +1,18 @@
 package configserver;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@EnableEurekaClient
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+
 }
